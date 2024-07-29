627f71931cc026fd4a64eb75af19c0b679136fc1
Ahmed <ahmdhusam7@gmail.com>
2024-07-29 14:36:46 +0300

    Feat: :sparkles: Prepare payments service functionality.

--- apps/payments/src/payments.controller.ts
+++ apps/payments/src/payments.controller.ts
@@ -1,12 +1,39 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, ParseIntPipe } from '@nestjs/common';
 import { PaymentsService } from './payments.service';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { PaymentsServiceEvents } from '@app/core/enums/payments-service.events.enum';
 
 @Controller()
 export class PaymentsController {
   constructor(private readonly paymentsService: PaymentsService) {}
 
-  @Get()
-  getHello(): string {
-    return this.paymentsService.getHello();
+  @MessagePattern(PaymentsServiceEvents.CREATE_ORDER)
+  async createOrder(@Payload(ParseIntPipe) amount: number) {
+    return this.paymentsService.createOrder(amount);
+  }
+
+  @MessagePattern(PaymentsServiceEvents.CHECK_AUTHORIZATION)
+  async checkAuthorization(@Payload() orderId: string) {
+    return this.paymentsService.checkAuthorization(orderId);
+  }
+
+  @MessagePattern(PaymentsServiceEvents.GET_ORDER_DATA)
+  async getOrderData(@Payload() orderId: string) {
+    return this.paymentsService.getOrderDetails(orderId);
+  }
+
+  @MessagePattern(PaymentsServiceEvents.GET_AUTHORIZE_PAYMENT)
+  async getAuthorizePayment(@Payload() authorizationId: string) {
+    return this.paymentsService.getAuthorizedPaymentDetails(authorizationId);
+  }
+
+  @MessagePattern(PaymentsServiceEvents.VOID_AUTHORIZATION)
+  async voidAuthorization(@Payload() authorizationId: string) {
+    return this.paymentsService.voidAuthorization(authorizationId);
+  }
+
+  @MessagePattern(PaymentsServiceEvents.CAPTURE_PAYMENT)
+  async capturePayment(@Payload() authorizationId: string) {
+    return this.paymentsService.capturePayment(authorizationId);
   }
 }
--- apps/payments/src/payments.service.ts
+++ apps/payments/src/payments.service.ts
@@ -1,8 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotImplementedException } from '@nestjs/common';
 
 @Injectable()
 export class PaymentsService {
-  getHello(): string {
-    return 'Hello World!';
+  async createOrder(amount: number) {
+    throw new NotImplementedException();
+  }
+
+  async checkAuthorization(orderId: string) {
+    throw new NotImplementedException();
+  }
+
+  async getOrderDetails(orderId: string) {
+    throw new NotImplementedException();
+  }
+
+  async getAuthorizedPaymentDetails(authorizationId: string) {
+    throw new NotImplementedException();
+  }
+
+  async voidAuthorization(authorizationId: string) {
+    throw new NotImplementedException();
+  }
+
+  async capturePayment(authorizationId: string) {
+    throw new NotImplementedException();
   }
 }
